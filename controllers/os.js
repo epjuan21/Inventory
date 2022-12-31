74578c8a2bba9a34b5b815dbf05ed3f365f4f047
Juan Ramirez <ep.juan@gmail.com>
2022-12-31 18:29:13 -0500

    Crear funcion eliminar y obtener sistema operativo

--- controllers/os.js
+++ controllers/os.js
@@ -1,11 +1,37 @@
-const { uploadImage, clearTemp } = require("../lib/cloudinary");
+const { uploadImage, clearTemp, deleteImage } = require("../lib/cloudinary");
 const { handleHttpError, handleHttpSuccess } = require("../lib/handleHttpResponse");
 const Os = require("../models/Os");
 
 // Obtener todos los Sistemas Operativos
 const getOs = async (req, res) => {
     try {
-        const os = await Os.find({ state: true });
+        // const os = await Os.find({ state: true });
+        const os = await Os.aggregate(
+            [
+                {
+                    $lookup: {
+                        from: "workstations",
+                        localField: "workstation_id",
+                        foreignField: "_id",
+                        as: "workstation"
+                    }
+                },
+                { $unwind: "$workstation" },
+                {
+                    $project: {
+                        _id: 0,
+                        licence: 1,
+                        version: 1,
+                        barcode: 1,
+                        serial: 1,
+                        workstation: "$workstation.description",
+                        availability: 1,
+                        consecutive: 1,
+                        image: 1
+                    }
+                }
+            ]
+        )
         if (!os) return handleHttpError(res, `No hay sistemas operativos`, 404);
         handleHttpSuccess(res, os, 200);
     } catch (error) {
@@ -38,7 +64,27 @@ const createOs = async (req, res) => {
     }
 }
 
+// Eliminar Sistema Operativo
+const deleteOs = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const os = await Os.findByIdAndUpdate(id, { state: false });
+        if (!os) return handleHttpError(res, `No se encontro el sistema operativo`, 404);
+
+        // Eliminar Imagen de Cloudinary
+        if (os.image) {
+            const image = os.image;
+            await deleteImage(image.public_id);
+        }
+
+        handleHttpSuccess(res, os, 200);
+    } catch (error) {
+        handleHttpError(res, error.message, 500);
+    }
+}
+
 module.exports = {
     getOs,
-    createOs
+    createOs,
+    deleteOs
 }
